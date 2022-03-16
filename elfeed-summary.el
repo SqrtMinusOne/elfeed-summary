e0488516a99e7cb5b6a4e9d4cbdd437eddbdaf0c
SqrtMinusOne <thexcloud@gmail.com>
2022-03-16 10:51:56 +0300

    initial commit

---
+++ elfeed-summary.el
@@ -0,0 +1,33 @@
+;;; elfeed-summary.el --- TODO -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Korytov Pavel
+
+;; Author: Korytov Pavel <thexcloud@gmail.com>
+;; Maintainer: Korytov Pavel <thexcloud@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1") (magit-section "3.3.0") (elfeed "3.4.1"))
+;; Homepage: https://github.com/SqrtMinusOne/elfeed-summary.el
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; TODO
+
+;;; Code:
+(require 'elfeed)
+
+(provide 'elfeed-summary)
+;;; elfeed-summary.el ends here
